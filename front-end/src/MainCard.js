4292effefd901d0e43a3068c1968e088080b311c
Paul Onderisin <paulonderisin@Pauls-MacBook-Air.local>
2023-02-28 13:52:46 -0500

    created URLS for each card based on deviceID and a predone feed

--- front-end/src/MainCard.js
+++ front-end/src/MainCard.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import Card from '@mui/material/Card';
-import { Typography } from "@mui/material";
+import { CardActionArea, Typography } from "@mui/material";
 
 const MainCard = props => {
 
@@ -9,14 +9,18 @@ const MainCard = props => {
 
     const oddEven = props.index % 2 == 0 ? "red":"blue";
 
+    const url = "https://io.adafruit.com/speedlights/feeds/" + deviceID;
+
     return (
       <Card style={{backgroundColor: oddEven}}>
-        <Typography display="block">Plant Name: {plantName}</Typography>
-        <Typography display="block">Device ID: {deviceID}</Typography>
-        <Typography display="block">Times per Day to Water: {timesPerDay}</Typography>
-        <Typography display="block">TImees per Week to Water: {timesPerWeek}</Typography>
-        <Typography display="block">Durination of Water: {durination}</Typography>
-        <button onClick={props.onRemove}>Remove Card</button>
+        <CardActionArea href= {url} target="_blank" >
+          <Typography display="block">Plant Name: {plantName}</Typography>
+          <Typography display="block">Device ID: {deviceID}</Typography>
+          <Typography display="block">Times per Day to Water: {timesPerDay}</Typography>
+          <Typography display="block">TImees per Week to Water: {timesPerWeek}</Typography>
+          <Typography display="block">Durination of Water: {durination}</Typography>
+          <button onClick={props.onRemove}>Remove Card</button>
+        </CardActionArea>
       </Card>
     );
   };

99dc799ec42739b5c5ae1060b6c093a9f0a6d6e8
dswit3 <57125291+dswit3@users.noreply.github.com>
2023-03-02 13:32:13 -0500

    Update MainCard.js

--- front-end/src/MainCard.js
+++ front-end/src/MainCard.js
@@ -7,7 +7,7 @@ const MainCard = props => {
 
     const [plantName, deviceID, timesPerDay, timesPerWeek, durination, cardID] = props.number;
 
-    const oddEven = props.index % 2 == 0 ? "red":"blue";
+    const oddEven = props.index % 2 == 0 ? "#ffcdd2":"#81d4fa";
 
     const feedname = deviceID == "abc_1"? "moist":"moistier";
 
@@ -15,11 +15,11 @@ const MainCard = props => {
 
     return (
       <Card style={{backgroundColor: oddEven}}>
-          <Typography display="block">Plant Name: {plantName}</Typography>
-          <Typography display="block">Device ID: {deviceID}</Typography>
+          <Typography display="block" align="center" variant="h3" color="green">{plantName}</Typography>
           <Typography display="block">Times per Day to Water: {timesPerDay}</Typography>
-          <Typography display="block">Times per Week to Water: {timesPerWeek}</Typography>
+          <Typography display="block">TImees per Week to Water: {timesPerWeek}</Typography>
           <Typography display="block">Durination of Water: {durination}</Typography>
+          <Typography display="block" align = "right">Device ID: {deviceID}</Typography>
           <button onClick={props.onRemove}>Remove Card</button>
           <form action={url} target="_blank">
             <button type="submit">View Chart</button>

c97ffbe2c74effc3271494468b2bee87cb255718
dswit3 <57125291+dswit3@users.noreply.github.com>
2023-03-21 13:04:55 -0400

    Cards look nice

--- front-end/src/MainCard.js
+++ front-end/src/MainCard.js
@@ -2,12 +2,15 @@
 import React, { useState } from "react";
 import Card from '@mui/material/Card';
 import { CardActionArea, Typography } from "@mui/material";
-
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import DefaultPlant from './images/article-cal-s.png';
 const MainCard = props => {
 
     const [plantName, deviceID, timesPerDay, timesPerWeek, durination, cardID] = props.number;
 
     const oddEven = props.index % 2 == 0 ? "#ffcdd2":"#81d4fa";
+    const NameColor = props.index %2 == 0? "#ef5350":"#2196f3"
 
     const feedname = deviceID == "abc_1"? "moist":"moistier";
 
@@ -15,16 +18,19 @@ const MainCard = props => {
 
     return (
       <Card style={{backgroundColor: oddEven}}>
-          <Typography display="block" align="center" variant="h3" color="green">{plantName}</Typography>
-          <Typography display="block">Times per Day to Water: {timesPerDay}</Typography>
-          <Typography display="block">TImees per Week to Water: {timesPerWeek}</Typography>
-          <Typography display="block">Durination of Water: {durination}</Typography>
-          <Typography display="block" align = "right">Device ID: {deviceID}</Typography>
-          <button onClick={props.onRemove}>Remove Card</button>
-          <form action={url} target="_blank">
-            <button type="submit">View Chart</button>
-          </form>
-
+      
+          <Typography display="block" align="center" variant="h3" style={{color: NameColor}}>{plantName}</Typography>
+          <Typography display="block" align="left" variant="h5">Water Scheduling: </Typography>
+          <Typography display="block">Per Day: {timesPerDay}</Typography>
+          <Typography display="block">Per Week: {timesPerWeek}</Typography>
+          <Typography display="block">Duration: {durination} sec</Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" style={{backgroundColor: NameColor}} onClick={props.onRemove}>Remove Card</Button>
+            <form action={url} target="_blank">
+              <Button variant="contained" style={{backgroundColor: NameColor}} type="submit">View Chart</Button>
+            </form>
+          </Stack>
+          <Typography display="block" align="right">Device ID: {deviceID}</Typography>
       </Card>
     );
   };
